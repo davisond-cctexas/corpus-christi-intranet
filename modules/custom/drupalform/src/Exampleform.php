6022a44038e4d0265b5c0d0df383e89f0a0e244a
root <root@web01-prd-cctx-app01.ccpd.net>
2018-04-11 11:09:10 -0500

    export db production

--- modules/custom/drupalform/src/Exampleform.php
+++ modules/custom/drupalform/src/Exampleform.php
@@ -9,48 +9,7 @@ use Drupal\Core\Form\FormStateInterface;
 
 
 
-class Exampleform extends FormBase
-{
 
-    /**
-     * Returns a unique string identifying the form.
-     *
-     * @return string
-     *   The unique string identifying the form.
-     */
-    public function getFormId()
-    {
-        // TODO: Implement getFormId() method.
-    }
 
-    /**
-     * Form constructor.
-     *
-     * @param array $form
-     *   An associative array containing the structure of the form.
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *   The current state of the form.
-     *
-     * @return array
-     *   The form structure.
-     */
-    public function buildForm(array $form, FormStateInterface $form_state)
-    {
-        // TODO: Implement buildForm() method.
-    }
-
-    /**
-     * Form submission handler.
-     *
-     * @param array $form
-     *   An associative array containing the structure of the form.
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *   The current state of the form.
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
-        // TODO: Implement submitForm() method.
-    }
-}
 
 
